6fc2087fa73e1e2adc4ce4274bf3c300f2d13f25
Ren Wang <iamwrm@gmail.com>
2022-02-06 13:31:35 +0800

    debuging vcpkg

--- ci/vcpkg_install.sh
+++ ci/vcpkg_install.sh
@@ -1,7 +1,8 @@
 git clone https://github.com/Microsoft/vcpkg.git
 ./vcpkg/bootstrap-vcpkg.sh
 ln -sf ./vcpkg/vcpkg /usr/local/bin/vcpkg
+ls -lah /usr/local/bin/
 
-vcpkg install folly
-vcpkg install fmt
-vcpkg install range-v3
+./vcpkg/vcpkg install folly
+./vcpkg/vcpkg install fmt
+./vcpkg/vcpkg install range-v3
